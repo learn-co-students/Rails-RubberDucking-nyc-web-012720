3923a7c5c8c992ef8b47a3bd2896fd553ebde108
davidmlee4 <davidmlee4@gmail.com>
2020-02-26 12:25:19 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,4 +4,50 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.create(student_params)
+
+    if @student.valid?
+      redirect_to @student
+    else
+      flash[:errors] = @student.errors.full_messages
+      redirect_to new_student_path
+    end
+
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    if @student.update(student_params)
+      redirect_to @student 
+    else
+      flash[:errors] = @student.errors.full_messages
+      redirect_to edit_student_path
+    end
+
+  end
+
+  def destroy
+    @student = Student.find(params[:id])
+    @student.destroy
+    redirect_to students_path
+  end
+
+  private
+  def student_params 
+    params.require(:student).permit(:name, :mod)
+  end
+
 end
