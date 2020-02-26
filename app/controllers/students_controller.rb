5a02c0784e03d373632f7819963a2862fc261271
wags83 <gpwagner@gmail.com>
2020-02-26 11:08:19 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,4 +4,51 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      flash[:success] = "Student successfully created"
+      redirect_to @student
+    else
+      flash[:error] = "Something went wrong"
+      render :new
+    end
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+  
+  def update
+    @student = Student.find(params[:id])
+      if @student.update(student_params)
+        flash[:success] = "Student was successfully updated"
+        redirect_to @student
+      else
+        flash[:error] = "Something went wrong"
+        render :edit
+      end
+  end
+  
+  def destroy
+    @student = Student.find(params[:id])
+    @student.destroy
+    redirect_to students_path
+  end
+  
+  private
+
+  def student_params
+    params.require(:student).permit(:name, :mod)
+  end
+  
+  
 end
