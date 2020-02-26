9f8ad2b56f5e69f105abe5251a0bb1760d779e0b
github username <github email address>
2020-02-26 13:41:48 -0500

    Done.

--- app/controllers/ducks_controller.rb
+++ app/controllers/ducks_controller.rb
@@ -1,7 +1,48 @@
 class DucksController < ApplicationController
+  before_action :set_duck, only: [:show, :edit, :update]
+
+  def new 
+    @duck = Duck.new
+  end 
+
+  def create 
+    @duck = Duck.create(duck_params)
+    if @duck.valid?
+      redirect_to @duck
+    else
+      flash[:errors] = @duck.errors.full_messages
+      redirect_to new_duck_path
+    end 
+  end 
 
   def index
     @ducks = Duck.all
   end
+
+  def show
+  end 
+
+  def edit
+  end 
+
+  def update
+    @duck.update(duck_params)
+    if @duck.valid?
+      redirect_to @duck
+    else
+      flash[:errors] = @duck.errors.full_messages
+      redirect_to edit_duck_path
+    end 
+  end 
+
+  private 
+
+  def duck_params 
+    params.require(:duck).permit(:name, :description, :student_id)
+  end 
+
+  def set_duck
+    @duck = Duck.find(params[:id])
+  end 
 
 end
--- app/models/duck.rb
+++ app/models/duck.rb
@@ -1,2 +1,13 @@
 class Duck < ApplicationRecord
+    belongs_to :student
+    validates :name, presence: true
+    validates :description, length: { maximum: 100 }
+
+    def student_name=(name)
+        self.student = Student.find_or_create_by(name: name)
+    end
+    
+    def student_name 
+        self.student ? self.student.name : nil
+    end 
 end
